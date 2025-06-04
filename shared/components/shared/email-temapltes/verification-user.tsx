7446702d36c59545f53fc4c057df77af92c8e8b2
St0b <ivanov24kaka@gmail.com>
2025-06-04 19:52:47 +0300

    Update verification-user.tsx

--- shared/components/shared/email-temapltes/verification-user.tsx
+++ shared/components/shared/email-temapltes/verification-user.tsx
@@ -11,7 +11,7 @@ export const VerificationUserTemplate: React.FC<Props> = ({ code }) => (
     </p>
 
     <p>
-      <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
+      <a href={`https://next-pizza-1-ivory.vercel.app/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
     </p>
   </div>
 );
